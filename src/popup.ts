8d66178870733eafd6cff91aa8c500b7aeb80aad
pfwrocks <pfwrocks@gmail.com>
2024-08-08 00:03:52 -0500

    refactor

--- src/popup.ts
+++ src/popup.ts
@@ -1,4 +1,5 @@
-import { render_flashcards } from "./component/render_flashcards";
+import { retrieve_flashcards } from "./core/access_saved_cards";
+import { Flashcard } from "./schema/flashcard";
 
 document
   .getElementById("generateButton")
@@ -22,3 +23,40 @@ chrome.storage.sync.onChanged.addListener((changes) => {
 
 // Initial update of stored content when popup opens
 document.addEventListener("DOMContentLoaded", render_flashcards);
+
+async function render_flashcards() {
+  const flashcards = await retrieve_flashcards();
+  const storedContentDiv = document.getElementById("storedContent");
+  if (!storedContentDiv) {
+    console.error("was unable to find the storedContent id in the DOM");
+    return;
+  }
+
+  if (flashcards && flashcards.length > 0) {
+    storedContentDiv.innerHTML = ""; // Clear existing content
+
+    const gridContainer = document.createElement("div");
+    gridContainer.className = "grid";
+
+    flashcards.forEach((flashcard: Flashcard) => {
+      const cardElement = document.createElement("article");
+      cardElement.className = "card";
+      if (flashcard.in_progress) {
+        cardElement.innerHTML = `
+                <p aria-busy="true">Card is being generated...</p>
+          `;
+      } else {
+        cardElement.innerHTML = `
+              ${flashcard.front}
+              <footer>${flashcard.back}</footer>
+          `;
+      }
+      gridContainer.appendChild(cardElement);
+    });
+
+    storedContentDiv.appendChild(gridContainer);
+  } else {
+    storedContentDiv.innerHTML = "<p>No flashcards stored yet.</p>";
+  }
+  console.log("Cards rerendered");
+}
